1de200904bd627772fdb976ae860f8d3ddf324d7
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-02 17:28:33 -0800

    Housekeeping

--- GiHubLibrary/GitOperations.cs
+++ GiHubLibrary/GitOperations.cs
@@ -11,56 +11,6 @@ namespace GiHubLibrary
 {
     public class GitOperations
     {
-        /// <summary>
-        /// Parse json file, only useful for development, will be deleted later.
-        /// </summary>
-        /// <param name="fileName"></param>
-        public static void Parse(string fileName)
-        {
-            var json = File.ReadAllText(fileName);
-            var repositories = JsonConvert.DeserializeObject<List<Repository>>(json);
-
-            foreach (var repository in repositories)
-            {
-                Console.WriteLine(repository.Name);
-            }
-
-            Console.WriteLine(repositories.Count);
-
-            Console.WriteLine(File.ReadAllLines(fileName).Length);
-        }
-
-        /// <summary>
-        /// TODO
-        ///   - refactor into for-next using count from Details()
-        ///   - placeholder for user name
-        ///   - token for page and per page
-        /// </summary>
-        public static void DownLoad()
-        {
-            //var request = WebRequest.Create("https://api.github.com/users/karenpayneoregon/repos?page=3&per_page=100; rel=\"last") as HttpWebRequest;
-            var request = WebRequest.Create("https://api.github.com/users/karenpayneoregon/repos?per_page=100") as HttpWebRequest;
-
-            if (request != null)
-            {
-                request.UserAgent = "TestApp";
-
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-                {
-                    var reader = new StreamReader(response.GetResponseStream());
-                    var json = reader.ReadToEnd();
-                    var repositories = JsonConvert.DeserializeObject<List<Repository>>(json);
-
-
-                    foreach (var repository in repositories)
-                    {
-                        Console.WriteLine(repository.Name);
-                    }
-
-                    Console.WriteLine(repositories.Count);
-                }
-            }
-        }
 
         public static async  Task<List<Repository>> DownLoadPublicRepositoriesAsync()
         {
