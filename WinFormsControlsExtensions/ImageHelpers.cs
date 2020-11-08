4936050cc32a7ac4924dbd324a6b9987b2c9a29f
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-08 03:22:10 -0800

    Refactors

---
+++ WinFormsControlsExtensions/ImageHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace WinFormsControlsExtensions
+{
+    public class ImageHelpers
+    {
+        /// <summary>
+        /// Converts a byte array to an image. If contents parameter is null return null
+        /// </summary>
+        /// <param name="contents">Valid byte array which represents an image usually from a database table</param>
+        /// <returns>An Image or null</returns>
+        public static Image ByteArrayToImage(byte[] contents)
+        {
+            if (contents is null)
+            {
+                return null;
+            }
+            else
+            {
+                var converter = new ImageConverter();
+                var image = (Image)converter.ConvertFrom(contents);
+
+                return image;
+            }
+        }
+
+
+    }
+}
