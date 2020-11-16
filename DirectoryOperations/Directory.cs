67d98cf5eb6649bfeda30e878ed3dfdbfd80fc78
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-16 05:02:10 -0800

    Added code sample for upcoming article on simple vs complex operation choices.

---
+++ DirectoryOperations/Directory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryOperations
+{
+    public class Directory
+    {
+        public delegate void OnDelete(string status);
+        /// <summary>
+        /// Callback for subscribers to see what is being worked on
+        /// </summary>
+        public static event OnDelete OnDeleteEvent;
+
+        public delegate void OnException(Exception exception);
+        /// <summary>
+        /// Callback for subscribers to know about a problem
+        /// </summary>
+        public static event OnException OnExceptionEvent;
+
+        /// <summary>
+        /// Recursively remove an entire folder structure and files with events for monitoring and basic
+        /// exception handling.
+        /// </summary>
+        /// <param name="currentDirectoryInfo"></param>
+        public static void RecursiveDelete(DirectoryInfo currentDirectoryInfo)
+        {
+            if (!currentDirectoryInfo.Exists)
+            {
+                OnDeleteEvent?.Invoke("Nothing to process");
+                return;
+            }
+
+            OnDeleteEvent?.Invoke(currentDirectoryInfo.Name);
+
+            foreach (var directoryInfo in currentDirectoryInfo.EnumerateDirectories())
+            {
+                try
+                {
+                    RecursiveDelete(directoryInfo);
+                }
+                catch (Exception ex)
+                {
+                    OnExceptionEvent?.Invoke(ex);
+                }
+            }
+
+            currentDirectoryInfo.Delete(true);
+
+        }
+        /// <summary>
+        /// Recursive remove an entire folder structure and files done by novice
+        /// coders who don't consider things like permissions on folders or files
+        /// that will deny an operation to remove a folder and or file or that
+        /// the folder or file is in use currently.
+        /// </summary>
+        /// <param name="folderName"></param>
+        public static void RecursiveDelete(string folderName)
+        {
+            foreach (var d in (new DirectoryInfo(folderName)).EnumerateDirectories("*.*", SearchOption.AllDirectories))
+            {
+                if (d.Name.Equals("Demo", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    d.Delete(recursive: true);
+                }
+            }
+        }
+    }
+}
---
+++ DirectoryOperations/readme.md
@@ -0,0 +1,8 @@
+﻿# About
+
+Code in this project shows operations done correctly and incorrectly although in some cases the incorrect opteration may be find for a task while in later task may run into runtime exceptions.
+
+For example, recursively removing an entire folder structure.
+
+* **Case one**, remove a folder structure under the users Document folder, unless an item is in use a simple method is fine.
+* **Case two**, remove a folder structyre under a protected folder structure by Windows permissions, the simple method without proper assertion and/or use of a try-catch statement can lead to a runtime exception being thrown.
