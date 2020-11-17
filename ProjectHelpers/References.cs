b23b578cb31d00878f38a50a59733cb7a559817c
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-17 09:45:19 -0800

    Added project reference help class project.

---
+++ ProjectHelpers/References.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Assembly;
+
+namespace ProjectHelpers
+{
+    public class References
+    {
+        /// <summary>
+        /// Get project references
+        /// </summary>
+        /// <returns>List of references for the calling project.</returns>
+        public static IEnumerable<string> CurrentAssemblyReferencedAssemblies()
+        {
+            var referencedAssemblies = new List<string>();
+
+            foreach (AssemblyName assembly in GetEntryAssembly()?.GetReferencedAssemblies())
+            {
+                referencedAssemblies.Add(assembly.ToString());
+            }
+
+            return referencedAssemblies.AsReadOnly().AsEnumerable();
+
+        }
+    }
+}
